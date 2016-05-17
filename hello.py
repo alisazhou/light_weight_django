c1fd62ebe126227a2eff82936d61e6034c22fc0a
alisazzz <alisa.zwy@gmail.com>
2016-05-16 22:42:38 -0400

    Use os.environ vars.

--- hello.py
+++ hello.py
@@ -14,11 +14,19 @@ urlpatterns = (
 
 
 # settings.py
+import os
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY='somesecretkey',
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
